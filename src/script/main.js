fdb82324c2dbb069631a5809498a15b78d553583
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-03-13 22:55:12 -0300

    Conclusão parcial (faltam adicionar mais jogos)

--- src/script/main.js
+++ src/script/main.js
@@ -10,15 +10,15 @@ function atividade1() {
                             <td style="border: none;"></td>
                             <td>8</td>
                             <td>:</td>
-                            <td><input id="game-1-n1" type="number" maxlength="1"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">1</span><input id="game-1-n1" type="number" maxlength="1"></td>
                             <td>=</td>
                             <td>1</td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n2"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">2</span><input type="number" id="game-1-n2"></td>
                             <td>x</td>
                             <td>1</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n3"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">3</span><input type="number" id="game-1-n3"></td>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
                         </tr>
@@ -44,17 +44,17 @@ function atividade1() {
                         <tr>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n4"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">4</span><input type="number" id="game-1-n4"></td>
                             <td>×</td>
                             <td>1</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n5"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">5</span><input type="number" id="game-1-n5"></td>
                             <td style="border: none;"></td>
                             <td>6</td>
                             <td>:</td>
                             <td>2</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n6"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">6</span><input type="number" id="game-1-n6"></td>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
                         </tr>
@@ -82,17 +82,17 @@ function atividade1() {
                             <td style="border: none;"></td>
                             <td>4</td>
                             <td>:</td>
-                            <td><input type="number" id="game-1-n7"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">7</span><input type="number" id="game-1-n7"></td>
                             <td>=</td>
                             <td>2</td>
                             <td style="border: none;"></td>
                             <td>9</td>
                             <td>:</td>
-                            <td><input type="number" id="game-1-n8"></td>
-                            <td>-</td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">8</span><input type="number" id="game-1-n8"></td>
+                            <td>=</td>
                             <td>9</td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n9"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">9</span><input type="number" id="game-1-n9"></td>
                         </tr>
                         <!-- Linha 6 -->
                         <tr>
@@ -114,17 +114,17 @@ function atividade1() {
                         </tr>
                         <!-- Linha 7 -->
                         <tr>
-                            <td><input type="number" id="game-1-n10"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">10</span><input type="number" id="game-1-n10"></td>
                             <td>:</td>
                             <td>7</td>
                             <td>=</td>
                             <td>1</td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n11"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">11</span><input type="number" id="game-1-n11"></td>
                             <td>x</td>
                             <td>3</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n12"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">12</span><input type="number" id="game-1-n12"></td>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
@@ -152,19 +152,19 @@ function atividade1() {
                         <tr>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n13"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">13</span><input type="number" id="game-1-n13"></td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n14"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">14</span><input type="number" id="game-1-n14"></td>
                             <td>:</td>
-                            <td><input type="number" id="game-1-n15"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">15</span><input type="number" id="game-1-n15"></td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n16"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">16</span><input type="number" id="game-1-n16"></td>
                             <td style="border: none;"></td>
                             <td>0</td>
                             <td>x</td>
                             <td>7</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n17"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">17</span><input type="number" id="game-1-n17"></td>
                         </tr>
                         <!-- Linha 10 -->
                         <tr>
@@ -192,15 +192,15 @@ function atividade1() {
                             <td>:</td>
                             <td>1</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n18"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">18</span><input type="number" id="game-1-n18"></td>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
                             <td style="border: none;"></td>
-                            <td><input type="number" id="game-1-n19"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">19</span><input type="number" id="game-1-n19"></td>
                             <td>:</td>
                             <td>5</td>
                             <td>=</td>
-                            <td><input type="number" id="game-1-n20"></td>
+                            <td><span style="position: absolute; top: 0; left: 0; font-size: 10px; color: gray;">20</span><input type="number" id="game-1-n20"></td>
                         </tr>
                     </table>
     `;
@@ -210,8 +210,8 @@ function atividade1() {
 document.getElementById("game-container").appendChild(atividade1());
 
 
-const abrirmodal = document.querySelector('#cliqueaqui')
-abrirmodal.addEventListener('click', () => {
+const abrirmodal = document.querySelector('#cliqueaqui');
+  abrirmodal.addEventListener('click', () => {
     const modal = document.createElement("div");
     modal.classList.add("bgmodal");
     modal.innerHTML = `
@@ -220,7 +220,36 @@ abrirmodal.addEventListener('click', () => {
             <div class="prof">
                 <img src="./src/assets/elza.png"></img>
             </div>
+            <button id="fecharModal">Fechar</button>
         </div>
-    `
-    document.querySelector('body').appendChild(modal)
-})
+    `;
+    document.querySelector('body').appendChild(modal);
+
+    const fecharmodal = document.querySelector('#fecharModal');
+    fecharmodal.addEventListener('click', () => {
+      document.querySelector('body').removeChild(modal);
+    });
+  });
+
+const respostasCorretas = [8, 3, 3, 2, 2, 3, 2, 1, 0, 7, 1, 3, 1, 2, 0.6, 3, 0, 7, 0, 0];
+
+  function verificarRespostas() {
+    let todasCorretas = true;
+
+    respostasCorretas.forEach((resposta, index) => {
+      const input = document.getElementById(`game-1-n${index + 1}`);
+      if (input) {
+        const valor = parseFloat(input.value);
+        if (valor === resposta) {
+          input.style.backgroundColor = 'lightgreen';
+        } else {
+          input.style.backgroundColor = 'lightcoral';
+          todasCorretas = false;
+        }
+      }
+    });
+
+    if (todasCorretas) {
+      alert('Parabéns! Você acertou todas as respostas!');
+    }
+  }
